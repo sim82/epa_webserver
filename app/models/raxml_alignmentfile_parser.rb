e64ddea0657a8beb5441a465a28a2c1647244d1e
denis <Denis.Krompass@campus.lmu.de>
2011-03-07 16:43:58 +0100

    Raxml alignment parser automatically renames non unique taxa; Fast,Heuristics,Bootstrap Checkbox dependencies; 1/16 default value for heuristics; ConvertToPhyloXML Bootstrap bug

--- app/models/raxml_alignmentfile_parser.rb
+++ app/models/raxml_alignmentfile_parser.rb
@@ -1,11 +1,12 @@
-
-require 'pty'
-require 'fasta_to_phylip'
+require 'rubygems'
+require 'popen4'
+require "#{RAILS_ROOT}/app/models/fasta_to_phylip.rb"
 
 class RaxmlAlignmentfileParser
   
-attr_reader :format, :valid_format, :error ,:data , :ali_length
+attr_reader :format, :valid_format, :error ,:data , :ali_length, :log
   def initialize(stream)
+    @log = []
     @filename = ""
     @data = []
     if stream.instance_of?(String) #because of testing
@@ -81,45 +82,73 @@ attr_reader :format, :valid_format, :error ,:data , :ali_length
   private
   def checkUniquenessOfNames
     names = {}
+    first_name = ""
     seqs = 0;
     seq_len = 0;
     read_names = false
+    data_copy = Array.new(@data)
     @data.each do |line|
       if line =~ /\s*(\d+)\s+(\d+)/
         seqs = $1.to_i
         seq_len = $2.to_i
       elsif line =~  /^\s+$/
         #all names have been read, quit procedure
+        #clear the names hash 
+        #rename again
         if read_names
+          read_names = false
+          if names.size > seqs
+            @data = data_copy
+            @log << "RAxML_Alignmentfile_parser couldn't read format! Reseted alignmentfile!"
+            return
+          end
+          names = {}
+        end
+      elsif line =~ /\s*\S+\s*/ #A sequence line is read
+        if names.size > seqs
+          @data = data_copy
+          @log << "RAxML_Alignmentfile_parser couldn't read format! Reseted alignmentfile! "
           return
         end
-      elsif line =~ /\s*\S+\s*/ 
-        read_names = true  
         s = line.gsub(/\s/, "")
         # check if line length without whitespaces matches the sequence length given 
         if s.length > seq_len  # Taxon and sequence in the line
           if line =~ /^\s*(\S+)\s+/
             name = $1
+            if !read_names 
+              if !first_name.eql?(name) && first_name != ""  # interleaved format without reoccurring taxon names
+                return
+              end
+              first_name = name
+              read_names = true  
+            end
             if names[name].nil?
               names[name] = 1
             else
               names[name] += 1
               line.sub!(/^\s*(\S+)/,name+"_"+names[name].to_s)
-              "renamed #{name} to #{name+"_"+names[name].to_s}"
+              @log << "renamed #{name} to #{name+"_"+names[name].to_s}"
             end
           else
              @error = "ERROR: couldn't parse #{line}"
              @valid_format = false
           end
-        elsif s.length < seq_len # Only Taxon in the line
+        elsif s.length < seq_len # Only Taxon in the line or interleaved format
           if line =~ /^\s*(\S+)\s*/
             name = $1
+            if !read_names 
+              if !first_name.eql?(name) && first_name != "" # interleaved format without reoccurring taxon names
+                return
+              end
+              first_name = name
+              read_names = true  
+            end
             if names[name].nil?
               names[name] = 1
             else
               names[name] += 1
               line.sub!(/^\s*(\S+)/,name+"_"+names[name].to_s)
-              puts "renamed #{name} to #{name+"_"+names[name].to_s}"
+              @log << "renamed #{name} to #{name+"_"+names[name].to_s}"
             end
           else
             @error =  "ERROR: couldn't parse #{line}"
@@ -142,18 +171,17 @@ attr_reader :format, :valid_format, :error ,:data , :ali_length
     f = File.open(file,'wb')
     @data.each {|d| f.write(d)}
     f.close
-    cmd = "#{RAILS_ROOT}/bioprogs/raxml/raxmlHPC  -s #{file} -fc -m GTRGAMMA -n test"
+    cmd = "#{RAILS_ROOT}/bioprogs/raxml/raxmlHPC-SSE3  -s #{file} -fc -m GTRGAMMA -n test"
     #let RAxML check if phylip format is correct
-    PTY.spawn(cmd) do |stdin, stdout, pid| 
-      
-      stdin.each do  |line| 
-        if !(line =~ /^Alignment\sformat\scan\sbe\sread\sby\sRAxML/) || !(line=~ /IMPORTANT\s+WARNING/)
+    POpen4::popen4( cmd ) do |stdout, stderr, stdin|
+      stdout.each do |line|
+        if !(line =~ /^Alignment\sformat\scan\sbe\sread\sby\sRAxML/) && !(line=~ /IMPORTANT\s+WARNING/)
           @error = @error+line
           @format = "unk"
           @valid_format = false
         end
       end
-    end rescue Errno::EIO
+    end 
     if !@error.eql?("")
       @error = "#{@message}\n#{@error}"
     end
